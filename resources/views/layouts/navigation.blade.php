f4225780e51eb3b1aaa465b92b08ab1df2d664b6
Nemes1sX <kazstonk@gmail.com>
2025-01-19 01:35:57 +0200

    Fix marketplace routes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('marketplaces.index')" :active="request()->routeIs('marketplaces.index')">
+                        {{ __('Marketplaces') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
