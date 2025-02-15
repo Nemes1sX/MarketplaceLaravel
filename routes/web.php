b8ededf9cdd637708894f66d348c28240d2150ce
Nemes1sX <kazstonk@gmail.com>
2025-02-16 01:51:24 +0200

    Add relations and routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MarketplaceController; 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/marketplaces', MarketplaceController::class)->names('marketplaces');
+    Route::resource('marketplace.products', ProductController::class)->middleware(['auth']);
 });
 
 require __DIR__.'/auth.php';
