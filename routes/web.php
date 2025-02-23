fe88106d77b04ab2e01d91045342d863082aa7b5
Nemes1sX <kazstonk@gmail.com>
2025-02-23 02:15:22 +0200

    small fixes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/marketplaces', MarketplaceController::class)->names('marketplaces');
-    Route::resource('marketplace.products', ProductController::class)->middleware(['auth']);
+    Route::resource('marketplace.products', ProductController::class);
 });
 
 require __DIR__.'/auth.php';
