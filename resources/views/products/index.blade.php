975b56fb0300e8f3b94406506bbe053bdcb6a917
Nemes1sX <kazstonk@gmail.com>
2025-04-07 06:55:54 +0300

    cart ui fixes

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Products - {{ $marketplace->name }}
+        </h2>
+    </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-between items-center mb-6">
-                <h2 class="text-2xl font-bold text-gray-800">Products - {{ $marketplace->name }}</h2>
                 <a href="{{ route('marketplace.products.create', $marketplace) }}" 
                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Add New Product
